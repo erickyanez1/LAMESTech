244b6b9d87385093f0ca179d51f90d04e182d2f7
MPamelaYugsi <mpyugsi@espe.edu.ec>
2021-06-24 00:42:24 -0500

    update

--- GENERALJAVACODE/GrupalProject/src/ec/edu/espe/lames/view/SystemDisplayLAMES.java
+++ GENERALJAVACODE/GrupalProject/src/ec/edu/espe/lames/view/SystemDisplayLAMES.java
@@ -28,6 +28,9 @@ public class SystemDisplayLAMES {
         System.out.println("9VBattery\tLEDDiodes\tResistor\tIntegratedCircuit");
         System.out.println("$3.00 - $2\t$0.25 - $0.50\t$0.20 - $0.30\t$0.80 - $1.50");
         System.out.println("A  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B\t");
+        System.out.println("\nDipswitch\tCapacitor\tPotentiometer\tTransistor\tProtoboard");
+        System.out.println("$0.70 - $1.20\t$0.60 - $1.00\t$0.80 - $1.25\t$0.70 - $1.00\t$18.00 - $5.00");
+        System.out.println("A  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B");
         
         for(int i = 0; i < option; i++){
             System.out.println("Enter the name of the Product");
