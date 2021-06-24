fbac24aa12f9d69981480926e8adb661f1987872
MPamelaYugsi <mpyugsi@espe.edu.ec>
2021-06-24 16:42:46 -0500

    update

--- GENERALJAVACODE/GrupalProject/src/ec/edu/espe/lames/view/SystemDisplayLAMES.java
+++ GENERALJAVACODE/GrupalProject/src/ec/edu/espe/lames/view/SystemDisplayLAMES.java
@@ -7,30 +7,46 @@ package ec.edu.espe.lames.view;
 
 import java.util.Scanner;
 import ec.edu.espe.lames.model.ElectronicMaterial;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
  * @author LAMESTech ESPE-DCC0
  */
 public class SystemDisplayLAMES {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("☻ Welcome to LAMESTech ☻");
-        
+        ElectronicMaterial electronicMaterial;
         String name;
         String fabricator;
         float cost;
+        String jsonElectronicMaterial = "";
+        FileWriter file = new FileWriter("./files/ElectronicMaterial.json");
         
         int option = 0;
         Scanner sn = new Scanner(System.in);
         
-        ElectronicMaterial em [] = new ElectronicMaterial[10];
-        
+        do{
         System.out.println("9VBattery\tLEDDiodes\tResistor\tIntegratedCircuit");
         System.out.println("$3.00 - $2\t$0.25 - $0.50\t$0.20 - $0.30\t$0.80 - $1.50");
         System.out.println("A  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B\t");
         System.out.println("\nDipswitch\tCapacitor\tPotentiometer\tTransistor\tProtoboard");
         System.out.println("$0.70 - $1.20\t$0.60 - $1.00\t$0.80 - $1.25\t$0.70 - $1.00\t$18.00 - $5.00");
         System.out.println("A  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B\t\tA  -  B");
+        
+        
+        switch(option){
+            case 1:
+                
+            case 2:
+            case 3:
+            default: 
+        }
+        }while(option != 3);
+        ElectronicMaterial em [] = new ElectronicMaterial[10];
+        
+        
         
         for(int i = 0; i < option; i++){
             System.out.println("Enter the name of the Product");
